194ea9533583ae6a958390ded207477cfb02448e
fabioandreslz <88564984+fabioandreslz@users.noreply.github.com>
2022-10-15 03:56:14 -0500

    Agregar archivos de proyecto.

---
+++ InterfazRes/InterfazRes/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InterfazRes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+       private async void Button_Clicked(object sender, EventArgs e)
+        {
+           
+            if (txtDocu.Text == "1013" && txtCont.Text == "123")
+            {
+                await DisplayAlert("Ingreso", "Bienvenido", "Aceptar");
+                await Navigation.PushModalAsync(new OrderPage());
+            }
+            else
+            {
+                await DisplayAlert("Error", "Los datos ingresados son incorrectos", "Aceptar");
+            }
+        }
+
+       
+    }
+}
---
+++ InterfazRes/InterfazRes/viewModel/OrderViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace InterfazRes.viewModel
+{
+    public class OrderViewModel : BaseViewModel
+    {
+        public OrderViewModel()
+        {
+            MenuList = GetMenu();
+        }
+        public List<Pick> MenuList { get; set; }
+        //public ICommand BackCommand => new Command(() => ApplicationException.Current.MainPage.Navigation.PopAsync());
+
+        public List<Pick> GetMenu()
+        {
+            return new List<Pick>
+            {
+                new Pick{Title="", Image="IMG01.png", Description="", Price=""},
+                new Pick{Title="", Image="IMG04.png", Description="", Price=""},
+                new Pick{Title="", Image="IMG05.png", Description="", Price=""}
+            };
+        }
+    }
+}
